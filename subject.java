86342d2f48620fa5c323db4869021bf1b01c5675
adinaturlybek <49300151+adinaturlybek@users.noreply.github.com>
2019-12-23 00:19:21 +0600

    Create subject.java

---
+++ subject.java
@@ -0,0 +1,10 @@
+package ObserverPattern;
+
+import Task2.Observer;
+
+public interface Subject {
+    void removeObserver(Observer o);
+    void registerObserver(Observer o);
+    void notifyObservers();
+}
+© 2019 GitHub, Inc.
